e3d727f2fcebd42729433f596e32b2a565570673
Darián López Utra <daroxs95@gmail.com>
2023-06-01 14:52:28 -0400

    Base page layout

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,45 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import logo from "./assets/teckstackify.svg";
+  import Counter from "./lib/Counter.svelte";
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+<nav>
+  <div class="content-centered w-100">
+    <a href="/" class="f-ai-center p-def">
+      <img src={logo} class="logo" alt="" width="200" />
     </a>
   </div>
-  <h1>Vite + Svelte</h1>
+</nav>
 
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+<main class="content-centered p-def">
+  <Counter />
 </main>
 
+<footer class="vstack">
+  <div class="hstack f-jc-center">
+    <a
+      href="https://github.com/daroxs95/techstackify"
+      target="_blank"
+      title="Github"
+    >
+      <i class="fa-brands fa-github font-icon-button" />
+    </a>
+  </div>
+  <!-- <div>
+    &copy; {today.getFullYear()}
+    {MY_NAME}. Maybe some rights reserved.
+  </div> -->
+</footer>
+
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
+  footer {
+    padding: 40px 25px;
+    text-align: center;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
+  .font-icon-button {
+    font-size: 40px;
   }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+  main {
+    min-height: 100vh;
   }
 </style>
